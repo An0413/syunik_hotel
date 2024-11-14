85b236d886e291b6844c57ebdc0e3751d6406001
HaykMargaryan82 <hayk.margaryan.1982@gmail.com>
2024-11-14 16:31:44 +0400

    layout create

---
+++ resources/views/site/layout/header.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Untree.co">
+
+
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+
+    <link href="{{asset('https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,500i,700|Roboto:300,400,500,700&display=swap')}}"
+          rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('css/css/vendor/icomoon/style.css')}}">
+    <!--   -->
+    <link rel="stylesheet" href="{{asset('css/css/vendor/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/css/scss/style.scss')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/style.css type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/flaticon.css type="text/css"')}}" >
+
+    <link rel="stylesheet" href="{{asset('css/sonCss/font-awesome.min.css" type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/elegant-icons.css" type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/flaticon.css type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/owl.carousel.min.css" type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/nice-select.css type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/jquery-ui.min.css type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/magnific-popup.css type="text/css"')}}">
+    <link rel="stylesheet" href="{{asset('css/sonCss/slicknav.min.css type="text/css"')}}">
+
+
+    <!-- deluxe -->
+    <link rel="stylesheet" href="{{asset('css/deluxeCss/style.css')}}">
+
+    <!--    <link rel="stylesheet" href="deluxeCss/open-iconic-bootstrap.min.css">-->
+    <!--    <link rel="stylesheet" href="deluxeCss/animate.css">-->
+
+    <!--    <link rel="stylesheet" href="deluxeCss/owl.carousel.min.css">-->
+    <!--    <link rel="stylesheet" href="deluxeCss/owl.theme.default.min.css">-->
+    <!--    <link rel="stylesheet" href="deluxeCss/magnific-popup.css">-->
+
+    <!--    <link rel="stylesheet" href="deluxeCss/aos.css">-->
+
+    <!--    <link rel="stylesheet" href="deluxeCss/ionicons.min.css">-->
+
+    <link rel="stylesheet" href="{{asset('css/deluxeCss/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('css/deluxeCss/jquery.timepicker.css')}}">
+
+
+    <!--    <link rel="stylesheet" href="deluxeCss/flaticon.css">-->
+    <!--    <link rel="stylesheet" href="deluxeCss/icomoon.css">-->
+    <title>LuxuryHotel Free HTML Template by Untree.co</title>
+</head>
+
+<body>
+
+<div id="untree_co--overlayer"></div>
+<div class="loader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+
+<nav class="untree_co--site-mobile-menu">
+    <div class="close-wrap d-flex">
+        <a href="#" class="d-flex ml-auto js-menu-toggle">
+            <span class="close-label">Close</span>
+            <div class="close-times">
+                <span class="bar1"></span>
+                <span class="bar2"></span>
+            </div>
+        </a>
+    </div>
+    <div class="site-mobile-inner"></div>
+</nav>
---
+++ resources/views/site/layout/layout.blade.php
@@ -0,0 +1,55 @@
+@include('site.layout.header')
+<div class="untree_co--site-wrap">
+        <!-- Navbar Start -->
+        <nav class="untree_co--site-nav js-sticky-nav">
+            <div class="container d-flex align-items-center">
+                <div class="logo-wrap">
+                    <a href="index.html" class="untree_co--site-logo">LuxuryHotel</a>
+                </div>
+                <div class="site-nav-ul-wrap text-center d-none d-lg-block">
+                    <ul class="site-nav-ul js-clone-nav">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li class="has-children">
+                            <a href="rooms.html">Rooms</a>
+                        </li>
+                        <li class="has-children">
+                            <a href="services.html">Services</a>
+                        </li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="icons-wrap text-md-right">
+
+                    <ul class="icons-top d-none d-lg-block">
+                        <li class="mr-4">
+                            <a href="#" class="js-search-toggle"><span class="icon-search2"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="icon-facebook"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="icon-twitter"></span></a>
+                        </li>
+                        <li>
+                            <a href="#"><span class="icon-instagram"></span></a>
+                        </li>
+                    </ul>
+
+                    <!-- Mobile Toggle -->
+                    <a href="#" class="d-block d-lg-none burger js-menu-toggle" data-toggle="collapse"
+                       data-target="#main-navbar">
+                        <span></span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar End -->
+        @yield('content')
+    @include('site.layout.footer')
+</div>
+</body>
+</html>
+
+
