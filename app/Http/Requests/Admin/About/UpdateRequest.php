ecca84bd4b8d08bdb70c851235b2191fe219a13d
An0413 <aleqsanyan.an@bk.ru>
2024-11-20 13:50:41 +0400

    about crud

---
+++ app/Http/Requests/Admin/About/UpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Admin\About;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'info_am' => 'string|nullable',
+            'info_ru' => 'string|nullable',
+            'info_en' => 'string|nullable',
+        ];
+    }
+}
