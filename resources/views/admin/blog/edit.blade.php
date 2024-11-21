c194233faf40df1e26a434c30ba75349a394d1e5
An0413 <aleqsanyan.an@bk.ru>
2024-11-21 13:34:32 +0400

    blog is ok

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -4,33 +4,89 @@
         <div class="row g-4">
             <div class="col-sm-12 col-xl-12 col-lg-12 col-md-12">
                 <div class="bg-light rounded h-100 p-4">
-                    <h6 class="mb-4">Խմբագրել սենյակ</h6>
+                    <h6 class="mb-4">
+                        @if($blog->lang_id == 1)
+                            Խմբագրել բլոգ
+                        @elseif($blog->lang_id == 2)
+                            Редактировать блог
+                        @else
+                            Edit blog
+                        @endif
+                    </h6>
                     <form action="{{route('blog_update', $blog->id)}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <label for="seq" class="form-label">Հերթականություն</label>
+                            <label for="seq" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Հերթականություն
+                                @elseif($blog->lang_id == 2)
+                                    Последовательность
+                                @else
+                                    Sequence
+                                @endif
+                            </label>
                             <input class="form-control" type="number" name="seq" value="{{$blog->seq}}"
                                    min="1">
                         </div>
                         <div class="mb-3">
-                            <label for="meta_keyword" class="form-label">Բանալի բառ</label>
-                            <input type="text" class="form-control"  name="meta_keyword" value="{{$blog->meta_keyword}}">
+                            <label for="meta_keyword" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Բանալի բառ
+                                @elseif($blog->lang_id == 2)
+                                    Ключевое слово
+                                @else
+                                    Keyword
+                                @endif
+                            </label>
+                            <input type="text" class="form-control" name="meta_keyword" value="{{$blog->meta_keyword}}">
                         </div>
                         <div class="mb-3">
-                            <label for="meta_description" class="form-label">Նկարագրություն</label>
-                            <input type="text" class="form-control" name="meta_description" value="{{$blog->meta_description}}">
+                            <label for="meta_description" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Նկարագրություն
+                                @elseif($blog->lang_id == 2)
+                                    Описание
+                                @else
+                                    Description
+                                @endif
+                            </label>
+                            <input type="text" class="form-control" name="meta_description"
+                                   value="{{$blog->meta_description}}">
                         </div>
                         <div class="mb-3">
-                            <label for="name" class="form-label">Անուն</label>
+                            <label for="name" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Անուն
+                                @elseif($blog->lang_id == 2)
+                                    Имя
+                                @else
+                                    Name
+                                @endif</label>
                             <input type="text" class="form-control" name="name" value="{{$blog->name}}">
                         </div>
                         <div class="mb-3">
-                            <label for="info" class="form-label">Տեղեկություն</label>
+                            <label for="info" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Տեղեկություն
+                                @elseif($blog->lang_id == 2)
+                                    Информация
+                                @else
+                                    Information
+                                @endif
+                            </label>
                             <textarea class="form-control" placeholder="նկարագիր"
                                       style="height: 150px;" name="info">{{$blog->info}}</textarea>
                         </div>
                         <div class="mb-3">
-                            <label for="image" class="form-label">Լուսանկար</label>
+                            <label for="image" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Լուսանկար
+                                @elseif($blog->lang_id == 2)
+                                    Фото
+                                @else
+                                    Photo
+                                @endif
+                            </label>
                             <input class="form-control" type="file" id="image" name="image"
                                    accept="image/jpeg, image/jpg, image/png, image/gif">
                         </div>
@@ -38,16 +94,38 @@
                             <img src="{{asset('images/blog/'.$blog->image)}}" alt="" class="show_img" id="img">
                         </div>
                         <div class="mb-3">
-                            <label for="video" class="form-label">Վիդեո</label>
+                            <label for="video" class="form-label">
+                                @if($blog->lang_id == 1)
+                                    Վիդեո
+                                @elseif($blog->lang_id == 2)
+                                    Видео
+                                @else
+                                    Video
+                                @endif
+                            </label>
                             <input type="file" class="form-control" id="video" name="video" accept="video/*">
                         </div>
                         <div class="mb-3">
                             <video controls class="blog_show_video" id="blog_video">
                                 <source src="{{asset('video/blog/'.$blog->video)}}" type="video/*">
-                                Ձեր բրաուզերը չի աջակցում վիդեոներին։
+                                @if($blog->lang_id == 1)
+                                    Ձեր բրաուզերը չի աջակցում վիդեոներին։
+                                @elseif($blog->lang_id == 2)
+                                    Ваш браузер не поддерживает видео.
+                                @else
+                                    Your browser does not support videos․
+                                @endif
                             </video>
                         </div>
-                        <button type="submit" class="btn btn-primary">Խմբագրել</button>
+                        <button type="submit" class="btn btn-primary">
+                            @if($blog->lang_id == 1)
+                                Խմբագրել
+                            @elseif($blog->lang_id == 2)
+                                Редактировать
+                            @else
+                                Edit
+                            @endif
+                        </button>
                     </form>
                 </div>
             </div>
