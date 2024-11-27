cc1a9da7cf6febc5faf89e63f5d9fe523ff93999
HaykMargaryan82 <hayk.margaryan.1982@gmail.com>
2024-11-27 11:31:35 +0400

    Rooms multilanguages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,5 +4,16 @@ return [
     'blog' => 'Blog',
     'amenities' => 'Hotel Amenities',
     'conditions' => 'Amenities',
+    'rooms' => 'Rooms',
+    'price' => 'Pernight',
+    'size' => 'Size',
+    'capacity' => 'Capacity',
+    'child' => 'Childs',
+    'bed' => 'Bed',
+    'floor' => 'Floor',
+    'services' => 'Wifi, Television, Bathroom,...',
+    'booking' =>'Booking Now',
+    'king' => 'King Beds',
+    'more' => 'More Details',
 
 ];
